be4749bdb5044821b428f01a027d918696c34c6d
Fenrir-dems <31958514+Fenrir-dems@users.noreply.github.com>
2017-09-14 17:03:14 +0200

    Update ram_140917_ex_1_2.swift

--- Exercices_1/ram_140917_ex_1_2.swift
+++ Exercices_1/ram_140917_ex_1_2.swift
@@ -1,124 +1,139 @@
 import Foundation
-//EXO 2
-//:----
+//Exo 1.2
 
-//Question 1:
+//Question 1
+print("----- Questions 1 et 2-----");
+print(" ");
 
-let listeEtudiants = ["Robert Martin MMI1","Anne Dupond I2M", "Jean Jacques GEII","Isabelle Laporte I2M", "Sophie Legrand GEII","Francois Gerard I2M","Alice Martin SRC" ]
+let tabEtudiants = ["Robert Martin MMI1", "Anne Dupond I2M", "Jean Jacques GEII", "Isabelle Laporte I2M", "Sophie Legrand GEII", "Françaois Gerard I2M", "Alice Martin SRC"];
 
-//Question 2:
+//Question 2
 
-let noteEtu = [
-	"Robert Martin":10,
-	"Anne Dupond":5, 
-	"Jean Jacques":11,
-	"Isabelle Laporte":8,	
-]
-for (nom,note) in noteEtu {
-	print("nom: \(nom) note: \(note)")
-	
+let premNoteEtu = ["Robert Martin": 10, "Anne DUpond": 5, "Jean Jacques": 11, "Isabelle laposte": 8];
+for(nom, note) in premNoteEtu
+{
+	print("Nom: \(nom) note: \(note)");
 }
 
-//Question 3:
+//Question 3
+print(" ");
+print("-----Questions 3 et 4-----");
+print(" ");
 
-let ToutesNoteEtu = [
-	"Robert Martin":[10, 13, 14, 8, 11],
-	"Anne Dupond":[5, 3, 10, 3, 7], 
-	"Jean Jacques":[11, 20, 17, 2, 3],
-	"Isabelle Laporte":[8, 10, 10, 11, 9]	
-]
+let ToutesNotes = ["Robert Martin": [10, 13, 14, 8, 11], 
+				  "Anne Dupond": [5, 3, 10, 3, 7],
+				  "Jean Jacques": [11, 20, 17, 2, 3],
+				  "Isabelle Laporte": [8, 10, 10, 11, 9]];
 
-//Question 4:
+//Question 4
 
-print("Moyenne\n")
-for (etu,tabN) in ToutesNoteEtu {
-	var s = 0
-	for n in tabN {
-		s = s + n
+for (etu, tabN) in ToutesNotes
+{
+	var somme = 0;
+	for note in tabN {
+		somme = somme + note;
 	}
-	let m = Double(s)/Double(tabN.count)
-	print(" nom: \(etu) note: \(m)")
+	
+	let moyenne = Double(somme)/Double(tabN.count);
+	print("\(etu): moyenne: \(moyenne)");
 }
 
-//Question 5:
+//Question 5
+print(" ");
+print("-----Question 5-----");
+print(" ");
 
-print("Max\n")
-for (etu,tabN) in ToutesNoteEtu {
-	var max = 0
-	for n in tabN {
-		if n > max {
-			max = n
+for (etu, tabN) in ToutesNotes
+{
+	var max = 0;
+	for note in tabN {
+		if note > max {
+			max = note;
 		}
 	}
-	print("nom: \(etu) note: \(max)")
+	print("\(etu): meilleure note: \(max)");
 }
 
-//Question 6:
+//Question 6
+print(" ");
+print("-----Question 6-----");
+print(" ");
 
-var mmi = [String]()
-var geii = [String]()
-var i2m = [String]()
+var mmi = [String]();
+var geii = [String]();
+var i2m = [String]();
+var src = [String]();
 
-for etud in listeEtudiants
+for etu in tabEtudiants
 {
-	switch etud {
-		case _ where etud.contains("GEII"):
-			print ("\(etud) ==> GEII")
-			geii.append(etud)
-		case _ where etud.contains("I2M"):
-			print ("\(etud) ==> I2M")
-			i2m.append(etud)
-		case _ where etud.contains("SRC"):
-			print ("\(etud) ==> SRC")
-		case _ where etud.contains("MMI"):
-			print ("\(etud) ==> MMI")
-			mmi.append(etud)
+	switch etu {
+		case _ where etu.contains("GEII"): //la fonction contains() necessite l'importation de Foundation
+			print("\(etu) ==> GEII");
+			geii.append(etu);
+		
+		case _ where etu.contains("MMI"): 
+			print("\(etu) ==> MMI");
+			mmi.append(etu);
+		
+		case _ where etu.contains("I2M"): 
+			print("\(etu) ==> I2M");
+			i2m.append(etu);
+		
+		case _ where etu.contains("SRC"): 
+			print("\(etu) ==> SRC");
+			src.append(etu);
+		
 		default:
-			print ("\(etud) ==> ????")
+			print("\(etu) ==> ??????");
 	}
 }
 
-//Question 7:
 
-print("Question 7\n")
+//Question 7
+print(" ");
+print("-----Question 7-----");
+print(" ");
 
-func moyenne(unTab: [Int])-> Double{
-	
-	
-	var s = 0
-	for n in unTab {
-		s = s + n
+func moyenne(unTab: [Int]) -> Double {
+	var somme = 0;
+	for note in unTab
+	{
+		somme = somme + note;
+			
 	}
-	let m = Double(s)/Double(unTab.count)
-	return m
+	let moyenne = Double(somme)/Double(unTab.count);
+	return moyenne;
 }
 
-
-func affStats(unDico: [String: [Int]], fStat: ([Int])-> Double) {
-	
+func affStats(unDico: [String: [Int]], fStat: ([Int]) -> Double) {
 	for (nom, notes) in unDico{
-	let st = fStat(notes)
-		print("nom:\(nom) : stat:\(st)")
+		let st = fStat(notes);
+		print("nom:\(nom) : stats: \(st)");
 	}
 }
-affStats(unDico: ToutesNoteEtu, fStat: moyenne)
 
-var test = [12, 23, 12, 10, 2, 13]
+affStats(unDico: ToutesNotes, fStat: moyenne);
 
-func s(a: Int, b:Int) -> Bool
-{ return a < b 
+//----méthode longue
+var test = [12, 15, 20, 5, 14, 3, 6];
+func s(a: Int, b: Int) -> Bool{
+	return a<b;
 }
-test.sort(by: s)
 
-//test.sort(by: { $0 < $1 })//Methode flemmard similaire a la func s
+test.sort(by: s);
+//----Fin méthode longue
 
-//Question 8:
+test.sort(by: {$0<$1}) //même résultat que "test.sort(by: s);" mais en mode SUPERFLEMMARD sans écrire la fonction
 
-print("Question 8\n")
+//Question 8
+print(" ");
+print("-----Question 8-----");
+print(" ");
 
-func mediane(unTab: [Int])-> Double{
-	var unTab = unTab
-	unTab.sort(by: {$0<$1})
+func mediane(unTab: [Int]) -> Double{
+	var unTab = unTab;
+	 unTab.sort(by: {$0<$1});
 	return Double(unTab[unTab.count/2])
 }
-affStats(unDico: ToutesNoteEtu, fStat: mediane)
+
+affStats(unDico: ToutesNotes, fStat: mediane);
